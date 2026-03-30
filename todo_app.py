ea0a08c2bed01ce76121a336962313d56f0d217a
mathew <mathew_holland@hotmail.co.uk>
2026-03-30 18:52:44 +0100

    Added a for loop using enumerate to print out the todos and todo inex, using start=1 to have the index show as 1 and not 0

--- todo_app.py
+++ todo_app.py
@@ -5,3 +5,6 @@ user_choice = input("Would you like to 'add', 'show', 'edit', 'complete' or 'exi
 if user_choice == 'add':
     todo_to_add = input("Enter your todo to add it to the todos list: ")
     todos.append(todo_to_add)
+elif user_choice == "show":
+    for todo_index, todo in enumerate(todos, start=1):
+        print(f"{todo_index}. {todo}")
