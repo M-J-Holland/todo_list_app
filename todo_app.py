223f491d97406428777421041679154d17e8395c
mathew <mathew_holland@hotmail.co.uk>
2026-03-30 18:11:32 +0100

    Added a welcome message, an empty todo list and added a condition for if user_choice == 'add'

---
+++ todo_app.py
@@ -0,0 +1,7 @@
+todos = []
+print("Welcome to the CLI TODO app!")
+
+user_choice = input("Would you like to 'add', 'show', 'edit', 'complete' or 'exit'? ").strip().lower()
+if user_choice == 'add':
+    todo_to_add = input("Enter your todo to add it to the todos list: ")
+    todos.append(todo_to_add)
