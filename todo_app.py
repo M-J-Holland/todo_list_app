1111e763c8cea893e63a06cd9485e86511705879
mathew <mathew_holland@hotmail.co.uk>
2026-03-30 19:43:49 +0100

    Added a option for editing the todos list which edits the number -1 (to take one from the user todo_to_edit to match the list indexing range 0 - x) in the todos list and replaces it with a new one.

--- todo_app.py
+++ todo_app.py
@@ -8,3 +8,14 @@ if user_choice == 'add':
 elif user_choice == "show":
     for todo_index, todo in enumerate(todos, start=1):
         print(f"{todo_index}. {todo}")
+elif user_choice == "edit":
+    todo_to_edit = input("Enter the number for the todo you would like to edit: ")
+    try:
+        todo_to_edit = int(todo_to_edit) - 1
+        if todo_to_edit < 0 or todo_to_edit >= len(todos):
+            print("Invalid todo number. Please try again.")
+        else:
+            new_todo = input("Enter your new todo to ")
+            todos[todo_to_edit] = new_todo
+    except ValueError:
+        print("Invalid input, please only enter a number")
