7baf3c001d9a0d1318d1b337d510d70ae70772e9
mathew <mathew_holland@hotmail.co.uk>
2026-03-31 19:49:02 +0100

    Small bug fixes and improved error handling. Added a check for empty todos list in 'show' and 'edit', created a way for the user to exit the app. and made some small changes with the edit to incorporate a while loop to allow the user to keep trying their edit

--- todo_app.py
+++ todo_app.py
@@ -1,33 +1,42 @@
 todos = []
 print("Welcome to the CLI TODO app!")
 
-user_choice = input("Would you like to 'add', 'show', 'edit', 'complete' or 'exit'? ").strip().lower()
-if user_choice == 'add':
-    todo_to_add = input("Enter your todo to add it to the todos list: ")
-    todos.append(todo_to_add)
-elif user_choice == "show":
-    for todo_index, todo in enumerate(todos, start=1):
-        print(f"{todo_index}. {todo}")
-elif user_choice == "edit":
-    todo_to_edit = input("Enter the number for the todo you would like to edit: ")
-    try:
-        todo_to_edit = int(todo_to_edit) - 1
-        if todo_to_edit < 0 or todo_to_edit >= len(todos):
-            print("Invalid todo number. Please try again.")
-        else:
-            new_todo = input("Enter your new todo to ")
-            todos[todo_to_edit] = new_todo
-    except ValueError:
-        print("Invalid input, please only enter a number")
-elif user_choice == 'complete':
-    todo_to_complete = input("Enter the number for the todo you would like to complete: ")
-    try:
-        todo_to_complete = int(todo_to_complete) -1
-        if todo_to_complete < 0 or todo_to_complete >= len(todos):
-            print("Invalid todo number. Please try again.")
-        else:
-            todos.pop(todo_to_complete)
-    except ValueError:
-        print("Invalid input, please only enter a number")
-elif user_choice == 'exit':
-    print("Thank you for using the TODO app, goodbye!")
+while True:
+    user_choice = input("Would you like to 'add', 'show', 'edit', 'complete' or 'exit'? ").strip().lower()
+    if user_choice == 'add':
+        todo_to_add = input("Enter your todo to add it to the todos list: ")
+        todos.append(todo_to_add)
+    elif user_choice == "show":
+        if not todos:
+            print("You have no todos to show, please add some todos first.")
+        for todo_index, todo in enumerate(todos, start=1):
+            print(f"{todo_index}. {todo}")
+    elif user_choice == "edit":
+            if not todos:
+                print("You have no todos to edit, please add some todos first.")
+            todo_to_edit = input("Enter the number for the todo you would like to edit: ")
+            while True:
+                try:
+                    todo_to_edit = int(todo_to_edit) - 1
+                    if todo_to_edit < 0 or todo_to_edit >= len(todos):
+                        print("Invalid todo number. Please try again.")
+                        todo_to_edit = input("Enter the number for the todo you would like to edit: ")
+                        continue
+                    new_todo = input("Enter your new todo: ")
+                    todos[todo_to_edit] = new_todo
+                    break
+                except ValueError:
+                    print("Invalid input, please only enter a number")
+                    todos_to_edit = input("Enter the number for the todo you would like to edit: ")
+    elif user_choice == 'complete':
+        todo_to_complete = input("Enter the number for the todo you would like to complete: ")
+        try:
+            todo_to_complete = int(todo_to_complete) -1
+            if todo_to_complete < 0 or todo_to_complete > len(todos):
+                print("Invalid todo number. Please try again.")
+            else:
+                todos.pop(todo_to_complete)
+        except ValueError:
+            print("Invalid input, please only enter a number")
+    elif user_choice == 'exit':
+        print("Thank you for using the TODO app, goodbye!")
